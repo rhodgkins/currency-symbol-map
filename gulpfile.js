e4c770fb96941df7563f65501a5b4de68ffb349f
Rich Hodgkins <rhodgkins@gmail.com>
2016-01-19 10:15:03 +0000

    Minified

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,14 @@ gulp.task('default', ['build']);
 gulp.task('build', function () {
 	var browserify = require('browserify'),
 		source = require('vinyl-source-stream'),
+		buffer = require('vinyl-buffer'),
+		uglify = require('gulp-uglify'),
 		options = { standalone: 'currency-symbol-map' };
 
 	return browserify(paths.src, options)
 		.bundle()
 		.pipe(source(pkg.name + '.js'))
+		.pipe(buffer())
+		.pipe(uglify())
 		.pipe(gulp.dest(paths.dest));
 });
